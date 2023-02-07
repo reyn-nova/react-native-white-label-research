c2a53eedfd1141971c8d0967789b03610d92751d
nald-dev <rynvva@gmail.com>
2023-02-07 14:45:25 +0700

    ios white label

---
+++ app/whitelabel/WhiteLabelConfig.tsx
@@ -0,0 +1,14 @@
+// WhiteLabelConfig.tsx
+
+import {NativeModules} from 'react-native';
+
+export default class WhiteLabelConfig {
+  public static readonly APP_NAME: string =
+    NativeModules.WhiteLabelConfig.getAppName();
+  public static readonly PRIMARY_COLOR: string =
+    NativeModules.WhiteLabelConfig.getPrimaryColor();
+  public static readonly PRIMARY_TEXT_COLOR: string =
+    NativeModules.WhiteLabelConfig.getPrimaryTextColor();
+  public static readonly GREETING_TEXT: string =
+    NativeModules.WhiteLabelConfig.getGreetingText();
+}
